c125219a8205b60133005995f1c5ebf8ca7903db
niggoo <f.nico@aon.at>
2018-03-01 20:21:40 +0100

    Update CsvMapper.ts

--- CsvMapper.ts
+++ CsvMapper.ts
@@ -10,3 +10,17 @@ export class CsvMapper {
         ).join('\n')
     }
 }
+
+
+/*
+Example usage:
+return CsvMapper.write<TaskDTO>({
+            "Id": t => t.id,
+            "Name": t => t.name,
+            "Description": t => t.description,
+            "Parent Task ID": t => t.parentTaskId,
+            "Priority": t => t.priority ? t.priority.name : null,
+            "Status": t => t.status ? t.status.name : null,
+            "Assigned to": t => t.assignedTo
+        }, tasks);
+*/
